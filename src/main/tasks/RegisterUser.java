5782ff9a853cd44753f87811b635177bc20c6910
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2019-05-13 19:34:43 -0500

    added post method

--- src/main/tasks/RegisterUser.java
+++ src/main/tasks/RegisterUser.java
@@ -1,4 +1,35 @@
 package tasks;
 
-public class RegisterUser {
+import io.restassured.http.ContentType;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.rest.interactions.Post;
+import net.serenitybdd.screenplay.rest.interactions.Put;
+
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class RegisterUser implements Task {
+
+    private final Object userInfo;
+
+    public RegisterUser(Object userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public static Performable withInfo(Object userInfo) {
+        return instrumented(RegisterUser.class, userInfo);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Post.to("/register").with(
+                        requestSpecification -> requestSpecification
+                                .contentType(ContentType.JSON)
+                                .body(userInfo)
+                )
+        );
+    }
 }
--- src/test/java/SerenityBddTests.java
+++ src/test/java/SerenityBddTests.java
@@ -1,4 +1,5 @@
 import models.users.Datum;
+import models.users.RegisterUserInfo;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abiities.CallAnApi;
@@ -8,6 +9,7 @@ import org.junit.runner.RunWith;
 import questions.GetUsersQuestion;
 import questions.ResponseCode;
 import tasks.GetUsers;
+import tasks.RegisterUser;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -46,4 +48,55 @@ public class SerenityBddTests {
                 seeThat("el avatar del usuario", act -> user.getAvatar(), equalTo("avatar"))
         );
     }
+
+    @Test
+    public void registerUserTest() {
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApiUrl));
+
+        String registerUserInfo = "{\n" +
+                "\t\"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\",\n" +
+                "    \"email\": \"tracey.ramos@reqres.in\",\n" +
+                "    \"password\": \"serenity\"\n" +
+                "}";
+
+        julian.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+
+        );
+
+        julian.should(
+                seeThat("el codigo de respuesta", new ResponseCode(), equalTo(200))
+        );
+
+    }
+
+    @Test
+    public void registerUserTest2() {
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApiUrl));
+
+
+        RegisterUserInfo registerUserInfo = new RegisterUserInfo();
+
+        registerUserInfo.setName("morpheus");
+        registerUserInfo.setJob("leader");
+        registerUserInfo.setEmail("tracey.ramos@reqres.in");
+        registerUserInfo.setPassword("serenity");
+
+        julian.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+
+        );
+
+        julian.should(
+                seeThat("el codigo de respuesta", new ResponseCode(), equalTo(200))
+        );
+
+
+
+    }
+
+
 }
