639425171c5fae1a36d0ea2a37209e5c429a7c7d
Julian <jdmesalosada@gmail.com>
2019-10-08 07:20:39 -0500

    adede readme

---
+++ src/main/java/com/testautomation/mesaj/interactions/database/ExecuteQuery.java
@@ -0,0 +1,12 @@
+package com.testautomation.mesaj.interactions.database;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Interaction;
+
+public class ExecuteQuery implements Interaction {
+
+    @Override
+    public <T extends Actor> void performAs(T t) {
+
+    }
+}
--- src/main/java/com/testautomation/mesaj/interactions/database/Select.java
+++ src/main/java/com/testautomation/mesaj/interactions/database/Select.java
@@ -2,7 +2,6 @@ package com.testautomation.mesaj.interactions.database;
 
 import com.testautomation.mesaj.abilities.InteractWithDb;
 import com.testautomation.mesaj.database.entity.Example;
-import com.testautomation.mesaj.interactions.Post;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 import net.thucydides.core.annotations.Step;
@@ -10,15 +9,16 @@ import net.thucydides.core.annotations.Step;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
-public class Select<T> implements Interaction {
+public class Select implements Interaction {
 
-    private final String query;
+    Class<?> clazz = null;
 
-    public Select(Class<T> cls ) {
-        this.query = query;
+    public Select(Class<?> clazz) {
+        this.clazz = clazz;
     }
 
     @Step("{0} executes a SELECT query #query")
@@ -27,7 +27,31 @@ public class Select<T> implements Interaction {
         EntityManager entityManager = InteractWithDb.as(actor).getManager();
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 
-        CriteriaQuery<Example> criteriaQuery = criteriaBuilder.createQuery(Example.class);
+        CriteriaQuery<?> criteriaQuery = criteriaBuilder.createQuery(clazz);
+        Root<?> root = criteriaQuery.from(clazz);
+
+         entityManager
+                .createQuery(criteriaQuery
+                        .select(root));
+
+        /*CriteriaDelete<Example> delete = criteriaBuilder.
+                createCriteriaDelete(Example.class);
+
+        Root e = delete.from(Example.class);
+        delete.where(criteriaBuilder.equal(e.get("id"), 1));
+        entityManager.createQuery(delete).executeUpdate();*/
+
+        /*
+        * EntityManager em = ...;
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Entity class> cq = cb.createQuery(Entity.class);
+        Root<Entity> from = cq.from(Entity.class);
+        cq.select(Entity);
+        TypedQuery<Entity> q = em.createQuery(cq);
+        List<Entity> allitems = q.getResultList()*/
+
+        //https://docs.jboss.org/hibernate/entitymanager/3.5/reference/en/html/querycriteria.html
+
     }
 
     public static Select to(String query) {
